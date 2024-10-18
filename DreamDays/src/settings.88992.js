8f85a3ec57e3dd96f117850cbd46f8c43b9ec736
Jacoyean <3290735152@qq.com>
2024-10-18 20:47:00 +0800

    场景【家】碰撞检测ok

--- DreamDays/src/settings.88992.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default","role","wall"],collisionMatrix:[[true],[false,false,true],[false,true,false]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Main.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"d0832",main:"0f1ee"}};
